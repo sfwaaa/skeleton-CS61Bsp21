c70f6c8a8916021108a58a839f89b8e0b9232284
sfwaaa <143580644+sfwaaa@users.noreply.github.com>
2026-09-09 21:44:41 +0800

    feat : Finish Lab3

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -22,6 +22,34 @@ public class TimeAList {
     }
 
     public static void timeAListConstruction() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> dataSize=new AList<>();
+        dataSize.addLast(1000);
+        dataSize.addLast(2000);
+        dataSize.addLast(8000);
+        dataSize.addLast(16000);
+        dataSize.addLast(32000);
+        dataSize.addLast(64000);
+        dataSize.addLast(128000);
+        //if using multiplicative resizing
+        //dataSize.addLast(12800000);
+        //{1000,2000,8000,16000,32000,64000};
+
+        AList<Double> times =new AList<Double>();
+
+        int insertVal=1;
+        Stopwatch timer =new Stopwatch();
+        double previousTime=0;
+        int currentSize=0;
+
+        AList<Integer> list=new AList<Integer>();
+        for (int sizeIndex=0;sizeIndex<dataSize.size();sizeIndex++){
+            currentSize=dataSize.get(sizeIndex);
+            previousTime=timer.elapsedTime();
+            for(int i=0;i<currentSize;i++){
+                list.addLast(insertVal);
+            }
+            times.addLast(timer.elapsedTime()-previousTime);
+        }
+        printTimingTable(dataSize,times,dataSize);
     }
 }
